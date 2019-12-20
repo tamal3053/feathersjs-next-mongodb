7cfa668c51591b480c7f70698629f4ced9e8ab61
tamal3053 <tamal.nemesis@gmail.com>
2019-12-20 21:14:46 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## FeathersJs + NextJs + MongoDB - Mongoose Boilerplate
---
+++ api/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+const logger = require("./logger");
+const app = require("./app");
+const nextApp = require("./nextApp").nextApp;
+const hostname = app.get("host");
+const port = app.get("port");
+
+nextApp.prepare().then(() => {
+  const server = app.listen(port, hostname);
+
+  process.on("unhandledRejection", (reason, p) =>
+    logger.error("Unhandled Rejection at: Promise ", p, reason)
+  );
+
+  server.on("listening", () =>
+    logger.info("Feathers application started on http://%s:%d", hostname, port)
+  );
+});
